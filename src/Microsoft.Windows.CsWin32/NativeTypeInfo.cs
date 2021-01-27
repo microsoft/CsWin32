5f0fda5a4ba4966f49ddfd4cb9a9bad299edc54a
Saul Rennison <saul.rennison@gmail.com>
2021-01-27 23:03:07 +0000

    #64: Project [Out] strings as StringBuilders

---
+++ src/Microsoft.Windows.CsWin32/NativeTypeInfo.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Windows.CsWin32
+{
+    internal readonly struct NativeTypeInfo
+    {
+        public NativeTypeInfo(UnmanagedType? unmanagedType, bool isNullTerminated, short? sizeParamIndex, int? sizeConst)
+        {
+            this.UnmanagedType = unmanagedType;
+            this.IsNullTerminated = isNullTerminated;
+            this.SizeParamIndex = sizeParamIndex;
+            this.SizeConst = sizeConst;
+        }
+
+        public UnmanagedType? UnmanagedType { get; }
+
+        public bool IsNullTerminated { get; }
+
+        public short? SizeParamIndex { get; }
+
+        public int? SizeConst { get; }
+    }
+}

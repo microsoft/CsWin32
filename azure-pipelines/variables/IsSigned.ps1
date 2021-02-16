c4a93d0827d76e6bdf5591372fd4917228adb7f9
Andrew Arnott <andrewarnott@live.com>
2021-02-16 14:23:54 -0700

    Get scheduled daily build and push to public feed

--- azure-pipelines/variables/IsSigned.ps1
+++ azure-pipelines/variables/IsSigned.ps1
@@ -1,11 +1,11 @@
 if ($env:SYSTEM_COLLECTIONID -eq 'cb55739e-4afe-46a3-970f-1b49d8ee7564') {
     if ($env:BUILD_REASON -eq 'Schedule') {
-      'real'
+      'true'
     } else {
-      if ($env:SIGNTYPESELECTION) {
-        $env:SIGNTYPESELECTION
+      if ($env:SIGNSELECTION) {
+        $env:SIGNSELECTION
       } else {
-        'test'
+        'false'
       }
     }
   }

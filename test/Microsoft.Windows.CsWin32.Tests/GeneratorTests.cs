c087ea21a35538175c2bb70f670b9d2729b3e6f8
Andrew Arnott <andrewarnott@live.com>
2021-03-16 16:15:55 -0600

    Workaround https://github.com/microsoft/win32metadata/issues/365

--- test/Microsoft.Windows.CsWin32.Tests/GeneratorTests.cs
+++ test/Microsoft.Windows.CsWin32.Tests/GeneratorTests.cs
@@ -65,7 +65,7 @@ public class GeneratorTests : IDisposable, IAsyncLifetime
     }
 
     [Theory]
-    [InlineData("CREATE_ALWAYS", "FILE_CREATE_FLAGS")]
+    [InlineData("COPYFILE2_CALLBACK_NONE", "COPYFILE2_MESSAGE_TYPE")]
     [InlineData("RTL_RUN_ONCE_ASYNC", null)]
     [InlineData("__zz__not_defined", null)]
     public void TryGetEnumName(string candidate, string? declaringEnum)

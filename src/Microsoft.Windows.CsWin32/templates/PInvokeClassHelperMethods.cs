d6cc2d928ab60941b8b0fd2b8069a554bcf2dcac
Andrew Arnott <andrew.arnott@microsoft.com>
2022-05-16 21:09:42 -0600

    Add null termination checks to `Span<char>` parameters
    Friendly overloads that use this instead of `PWSTR` should verify since it's easy for callers to believe the `Span<char>` length obviates the need for a null terminater that is still required by native code.

---
+++ src/Microsoft.Windows.CsWin32/templates/PInvokeClassHelperMethods.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// This class wrapper is stripped so that individual helper methods may be injected into the generated PInvoke class.
+/// </summary>
+internal class PInvokeClassHelperMethods
+{
+	private static void EnsureNullTerminated(Span<char> buffer, string parameterName)
+	{
+		if (buffer.Length == 0 || buffer[buffer.Length - 1] != '\0')
+		{
+			for (int i = buffer.Length - 2; i >= 0; i--)
+			{
+				if (buffer[i] == '\0')
+				{
+					return;
+				}
+			}
+
+			throw new ArgumentException("Required null terminator is missing.", parameterName);
+		}
+	}
+}

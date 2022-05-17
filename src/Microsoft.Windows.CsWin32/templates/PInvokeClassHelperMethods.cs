0143a4d8ded0cc4146e372431c014b0a4fa0bdc4
Andrew Arnott <andrew.arnott@microsoft.com>
2022-05-17 10:13:05 -0600

    Simplify `EnsureNullTerminated` helper method
    In fact at this point, the method is so simple it's probably better to just inline it. But maybe leaving it here as a placeholder for other helper methods is more worthwhile?

--- src/Microsoft.Windows.CsWin32/templates/PInvokeClassHelperMethods.cs
+++ src/Microsoft.Windows.CsWin32/templates/PInvokeClassHelperMethods.cs
@@ -5,16 +5,8 @@ internal class PInvokeClassHelperMethods
 {
 	private static void EnsureNullTerminated(Span<char> buffer, string parameterName)
 	{
-		if (buffer.Length == 0 || buffer[buffer.Length - 1] != '\0')
+		if (buffer.LastIndexOf('\0') == -1)
 		{
-			for (int i = buffer.Length - 2; i >= 0; i--)
-			{
-				if (buffer[i] == '\0')
-				{
-					return;
-				}
-			}
-
 			throw new ArgumentException("Required null terminator is missing.", parameterName);
 		}
 	}

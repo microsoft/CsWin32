b24037204e5b5395440db5dbaeabe3ae19c393db
elachlan <2433737+elachlan@users.noreply.github.com>
2022-07-30 16:38:12 +1000

    Add mapping to BCL types for POINT, POINTF, and SIZE

--- src/Microsoft.Windows.CsWin32/Generator.cs
+++ src/Microsoft.Windows.CsWin32/Generator.cs
@@ -50,6 +50,9 @@ public class Generator : IDisposable
         { "LARGE_INTEGER", PredefinedType(Token(SyntaxKind.LongKeyword)) },
         { "ULARGE_INTEGER", PredefinedType(Token(SyntaxKind.ULongKeyword)) },
         { "OVERLAPPED", ParseTypeName("global::System.Threading.NativeOverlapped") },
+        { "POINT", ParseTypeName("global::System.Drawing.Point") },
+        { "POINTF", ParseTypeName("global::System.Drawing.PointF") },
+        { "SIZE", ParseTypeName("global::System.Drawing.Size") },
     };
 
     internal static readonly Dictionary<string, TypeSyntax> AdditionalBclInteropStructsMarshaled = new Dictionary<string, TypeSyntax>(StringComparer.Ordinal)
@@ -3664,9 +3667,7 @@ public class Generator : IDisposable
             case "POINTF":
             case "POINT":
             case "RECT":
-            case "RECTF":
             case "SIZE":
-            case "SIZEF":
                 members.AddRange(this.ExtractMembersFromTemplate(name.Identifier.ValueText));
                 break;
             default:

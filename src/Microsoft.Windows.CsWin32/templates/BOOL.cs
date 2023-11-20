2b7732fb9dfb56bc946c8a3b77e1a84d836abe71
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2023-11-21 07:35:20 +1000

    Add helpful operators and constants to BOOL

--- src/Microsoft.Windows.CsWin32/templates/BOOL.cs
+++ src/Microsoft.Windows.CsWin32/templates/BOOL.cs
@@ -1,6 +1,11 @@
 ﻿partial struct BOOL
 {
+	public const int Size = sizeof(int);
+	public static BOOL TRUE { get; } = new(true);
+	public static BOOL FALSE { get; } = new(false);
 	internal BOOL(bool value) => this.Value = value ? 1 : 0;
 	public static implicit operator bool(BOOL value) => value.Value != 0;
 	public static implicit operator BOOL(bool value) => new BOOL(value);
+	public static bool operator true(BOOL value) => value.Value != 0;
+	public static bool operator false(BOOL value) => value.Value == 0;
 }

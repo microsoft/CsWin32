cfb467820e8d7378c1a4729d29cd132b20b549d6
Andrew Arnott <andrew.arnott@microsoft.com>
2024-09-30 11:46:32 -0600

    Fixes marshaling of structs with variable length arrays of `char`
    Fixes #1184

---
+++ src/Microsoft.Windows.CsWin32/templates/VariableLengthInlineArray`2.cs
@@ -0,0 +1,42 @@
+﻿internal struct VariableLengthInlineArray<T, TBlittable>
+	where T : unmanaged
+	where TBlittable : unmanaged
+{
+#if canUseUnscopedRef
+	private TBlittable _e0;
+
+	[UnscopedRef]
+	internal ref T e0 => ref Unsafe.As<TBlittable, T>(ref this._e0);
+
+#if canUseUnsafeAdd
+	internal ref T this[int index]
+	{
+		[UnscopedRef]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => ref Unsafe.Add(ref this.e0, index);
+	}
+#endif
+
+#if canUseSpan
+	[UnscopedRef]
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal Span<T> AsSpan(int length)
+	{
+#if canCallCreateSpan
+		return MemoryMarshal.CreateSpan(ref this.e0, length);
+#else
+		unsafe
+		{
+			fixed (void* p = &this.e0)
+			{
+				return new Span<T>(p, length);
+			}
+		}
+#endif
+	}
+#endif
+
+#else
+	internal TBlittable e0;
+#endif
+}
--- test/GenerationSandbox.Tests/FlexibleArrayTests.cs
+++ test/GenerationSandbox.Tests/FlexibleArrayTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
 using Windows.Win32.System.Ole;
 
 public class FlexibleArrayTests
@@ -33,5 +34,23 @@ public class FlexibleArrayTests
     {
         Assert.Equal(PAGESET.SizeOf(1), PAGESET.SizeOf(0));
         Assert.Equal(Marshal.SizeOf<PAGERANGE>(), PAGESET.SizeOf(2) - PAGESET.SizeOf(1));
+    }
+
+    [Fact]
+    [Trait("WindowsOnly", "true")]
+    public unsafe void NonBlittableType_Char()
+    {
+        // The argument values are not relevant here.
+        // The function is expected to return a failure error code in success cases.
+        // This is a regression test for https://github.com/microsoft/CsWin32/issues/1184,
+        // where the mere invocation of the function caused .NET Framework to throw a marshaling exception.
+        uint size = 0;
+        Windows.Win32.PInvoke.SetupDiGetDeviceInterfaceDetail(
+            new SafeFileHandle(IntPtr.Zero, false),
+            default,
+            null,
+            0,
+            &size,
+            null);
     }
 }

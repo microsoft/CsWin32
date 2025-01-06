dotnet/roslyn

Ensure Expand-Template uses UTF-8 when replacing placeholders.

Expand-Template.ps1
@@ -36,7 +36,7 @@ function Replace-Placeholders {
 
     $Path = Resolve-Path $Path
     Write-Host "Replacing tokens in `"$Path`""
-    $content = Get-Content -Path $Path | Out-String
+    $content = Get-Content -Encoding UTF8 -Path $Path | Out-String
     $Replacements.GetEnumerator() |% {
         $modifiedContent = $content -replace $_.Key,$_.Value
         if ($modifiedContent -eq $content) {

microsoft/CsWin32

Expand-Template.ps1
@@ -40,7 +40,7 @@ function Replace-Placeholders {
 
     $Path = Resolve-Path $Path
     Write-Host "Replacing tokens in `"$Path`""
-    $content = Get-Content -LiteralPath $Path | Out-String
+    $content = Get-Content -Encoding UTF8 -LiteralPath $Path | Out-String
     $Replacements.GetEnumerator() |% {
         $modifiedContent = $content -replace $_.Key,$_.Value
         if ($modifiedContent -eq $content) {
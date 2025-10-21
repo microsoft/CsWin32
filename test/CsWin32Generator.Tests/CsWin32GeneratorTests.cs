5ecc5db01eb18ed2eb6327837e4b71d4b0d08179
Jevan Saks <jevansa@microsoft.com>
2025-10-20 19:43:26 -0700

    ComVariant was not added until net9, emit VARIANT instead.

--- test/CsWin32Generator.Tests/CsWin32GeneratorTests.cs
+++ test/CsWin32Generator.Tests/CsWin32GeneratorTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#pragma warning disable SA1402
+#pragma warning disable SA1402,SA1201
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -101,61 +101,81 @@ public partial class CsWin32GeneratorTests : CsWin32GeneratorTestsBase
         Assert.Contains($"({signature})", generatedMemberSignatures);
     }
 
+    public static IList<object[]> TestApiData => [
+        ["CHAR", "Simple type"],
+        ["RmRegisterResources", "Simple function"],
+        ["IStream", "Interface with enum parameters that need to be marshaled as U4"],
+        ["IServiceProvider", "exercises out parameter of type IUnknown marshaled to object"],
+        ["CreateDispatcherQueueController", "Has WinRT object parameter which needs marshalling"],
+        ["IEnumEventObject", "Derives from IDispatch"],
+        ["DestroyIcon", "Exercise SetLastError on import"],
+        ["IDebugProperty", "DebugPropertyInfo has a managed field"],
+        ["GetThemeColor", "Tests enum marshaling to I4"],
+        ["ChoosePixelFormat", "Tests marshaled field in struct"],
+        ["IGraphicsEffectD2D1Interop", "Uses Uses IPropertyValue (not accessible in C#]"],
+        ["Folder3", "Derives from multiple interfaces"],
+        ["IAudioProcessingObjectConfiguration", "Test struct** parameter marshalling"],
+        ["IBDA_EasMessage", "[In,[Out, should not be added"],
+        ["ITypeComp", "ITypeComp should not be unmanaged"],
+        ["AsyncIConnectedIdentityProvider", "Needs [In,[Out, on array parameter"],
+        ["Column", "SYSLIB1092 that needs to be suppressed"],
+        ["IBidiAsyncNotifyChannel", "Parameter that needs special marshaling help"],
+        ["ID3D11Texture1D", "Unmanaged interface needs to not use `out` for any params"],
+        ["ID3D11DeviceContext", "ppClassInstances parameter is annotated to have a size from uint* parameter"],
+        ["IBrowserService2", "Array return type needs marshaling attribute"],
+        ["ID3D11VideoContext", "Parameter not properly annotated as marshaled"],
+        ["ID2D1Factory4", "Function matches more than one interface member. Which interface member is actually chosen is implementation-dependent. Consider using a non-explicit implementation instead"],
+        ["IDWriteFontFace5", "Pointers may only be used in an unsafe context"],
+        ["ICorProfilerCallback11", "already defines a member called 'SurvivingReferences' with the same parameter types"],
+        ["D3D11_VIDEO_DECODER_BUFFER_DESC1", "struct with pointer to struct"],
+        ["D3D11_VIDEO_DECODER_EXTENSION", "struct with array of managed types"],
+        ["IWICBitmap", "Interface with multiple methods with same name"],
+        ["ID3D12VideoDecodeCommandList1", "D3D12_VIDEO_DECODE_OUTPUT_STREAM_ARGUMENTS1 has an inline fixed length array of managed structs"],
+        ["IDebugHostContext", "Marshaling bool without explicit marshaling information is not allowed"],
+        ["HlinkCreateFromData", "IDataObject is not supported by source-generated COM"],
+        ["IVPNotify", "IVPNotify derives from an interface that's missing a GUID"],
+        ["PathGetCharType", "CharSet forwarded to another assembly"],
+        ["ItsPubPlugin", "The platform 'windowsserver' is not a known platform name"],
+        ["PFIND_MATCHING_THREAD", "Delegates can't have marshaling", TestOptions.GeneratesNothing],
+        ["LPFNDFMCALLBACK", "Delegate return value can't be marshaled", TestOptions.GeneratesNothing],
+        ["IDataObject", "Source generated COM can't rely on built-in IDataObject so make sure IDataObject generates correctly"],
+        ["JsCreateRuntime", "Delegate has bool parameter which should not get MarshalAs(bool] added to it in blittable mode"],
+        ["RoCreatePropertySetSerializer", "IPropertySetSerializer parameter doesn't get interface generated for it."],
+        ["D3D9ON12_ARGS", "D3D9ON12_ARGS has an inline object[2, array in a struct"],
+        ["Direct3DCreate9On12Ex", "D3D9ON12_ARGS argument is not supported for marshalling"],
+        ["IDWriteGdiInterop1", "InterfaceImplementation already declares ABI_GetFontSignature"],
+        ["IMFSinkWriterEx", "Introducing a 'Finalize' method can interfere with destructor invocation. Did you intend to declare a destructor?"],
+        ["RoRegisterActivationFactories", "The type 'delegate* unmanaged[Stdcall,<HSTRING, IActivationFactory_unmanaged**, HRESULT>' may not be used as a type argument"],
+        ["IMFMediaKeys", "cannot convert from 'Windows.Win32.Foundation.BSTR*' to 'object'"],
+        ["ICompositorInterop2", "Needs type from UAP contract that isn't available"],
+        ["SECURITY_NULL_SID_AUTHORITY", "static struct with embedded array incorrectly initialized"],
+        ["CreateThreadpoolWork", "Friendly overload differs only on return type and 'in' modifiers on attributes"],
+        ["GetModuleFileName", "Should have a friendly Span overload"],
+    ];
+
     [Theory]
-    [InlineData("CHAR", "Simple type")]
-    [InlineData("RmRegisterResources", "Simple function")]
-    [InlineData("IStream", "Interface with enum parameters that need to be marshaled as U4")]
-    [InlineData("IServiceProvider", "exercises out parameter of type IUnknown marshaled to object")]
-    [InlineData("CreateDispatcherQueueController", "Has WinRT object parameter which needs marshalling")]
-    [InlineData("IEnumEventObject", "Derives from IDispatch")]
-    [InlineData("DestroyIcon", "Exercise SetLastError on import")]
-    [InlineData("IDebugProperty", "DebugPropertyInfo has a managed field")]
-    [InlineData("GetThemeColor", "Tests enum marshaling to I4")]
-    [InlineData("ChoosePixelFormat", "Tests marshaled field in struct")]
-    [InlineData("IGraphicsEffectD2D1Interop", "Uses Uses IPropertyValue (not accessible in C#)")]
-    [InlineData("Folder3", "Derives from multiple interfaces")]
-    [InlineData("IAudioProcessingObjectConfiguration", "Test struct** parameter marshalling")]
-    [InlineData("IBDA_EasMessage", "[In][Out] should not be added")]
-    [InlineData("ITypeComp", "ITypeComp should not be unmanaged")]
-    [InlineData("AsyncIConnectedIdentityProvider", "Needs [In][Out] on array parameter")]
-    [InlineData("Column", "SYSLIB1092 that needs to be suppressed")]
-    [InlineData("IBidiAsyncNotifyChannel", "Parameter that needs special marshaling help")]
-    [InlineData("ID3D11Texture1D", "Unmanaged interface needs to not use `out` for any params")]
-    [InlineData("ID3D11DeviceContext", "ppClassInstances parameter is annotated to have a size from uint* parameter")]
-    [InlineData("IBrowserService2", "Array return type needs marshaling attribute")]
-    [InlineData("ID3D11VideoContext", "Parameter not properly annotated as marshaled")]
-    [InlineData("ID2D1Factory4", "Function matches more than one interface member. Which interface member is actually chosen is implementation-dependent. Consider using a non-explicit implementation instead")]
-    [InlineData("IDWriteFontFace5", "Pointers may only be used in an unsafe context")]
-    [InlineData("ICorProfilerCallback11", "already defines a member called 'SurvivingReferences' with the same parameter types")]
-    [InlineData("D3D11_VIDEO_DECODER_BUFFER_DESC1", "struct with pointer to struct")]
-    [InlineData("D3D11_VIDEO_DECODER_EXTENSION", "struct with array of managed types")]
-    [InlineData("IWICBitmap", "Interface with multiple methods with same name")]
-    [InlineData("ID3D12VideoDecodeCommandList1", "D3D12_VIDEO_DECODE_OUTPUT_STREAM_ARGUMENTS1 has an inline fixed length array of managed structs")]
-    [InlineData("IDebugHostContext", "Marshaling bool without explicit marshaling information is not allowed")]
-    [InlineData("HlinkCreateFromData", "IDataObject is not supported by source-generated COM")]
-    [InlineData("IVPNotify", "IVPNotify derives from an interface that's missing a GUID")]
-    [InlineData("PathGetCharType", "CharSet forwarded to another assembly")]
-    [InlineData("ItsPubPlugin", "The platform 'windowsserver' is not a known platform name")]
-    [InlineData("PFIND_MATCHING_THREAD", "Delegates can't have marshaling", TestOptions.GeneratesNothing)]
-    [InlineData("LPFNDFMCALLBACK", "Delegate return value can't be marshaled", TestOptions.GeneratesNothing)]
-    [InlineData("IDataObject", "Source generated COM can't rely on built-in IDataObject so make sure IDataObject generates correctly")]
-    [InlineData("JsCreateRuntime", "Delegate has bool parameter which should not get MarshalAs(bool) added to it in blittable mode")]
-    [InlineData("RoCreatePropertySetSerializer", "IPropertySetSerializer parameter doesn't get interface generated for it.")]
-    [InlineData("D3D9ON12_ARGS", "D3D9ON12_ARGS has an inline object[2] array in a struct")]
-    [InlineData("Direct3DCreate9On12Ex", "D3D9ON12_ARGS argument is not supported for marshalling")]
-    [InlineData("IDWriteGdiInterop1", "InterfaceImplementation already declares ABI_GetFontSignature")]
-    [InlineData("IMFSinkWriterEx", "Introducing a 'Finalize' method can interfere with destructor invocation. Did you intend to declare a destructor?")]
-    [InlineData("RoRegisterActivationFactories", "The type 'delegate* unmanaged[Stdcall]<HSTRING, IActivationFactory_unmanaged**, HRESULT>' may not be used as a type argument")]
-    [InlineData("IMFMediaKeys", "cannot convert from 'Windows.Win32.Foundation.BSTR*' to 'object'")]
-    [InlineData("ICompositorInterop2", "Needs type from UAP contract that isn't available")]
-    [InlineData("SECURITY_NULL_SID_AUTHORITY", "static struct with embedded array incorrectly initialized")]
-    [InlineData("CreateThreadpoolWork", "Friendly overload differs only on return type and 'in' modifiers on attributes")]
-    [InlineData("GetModuleFileName", "Should have a friendly Span overload")]
+    [MemberData(nameof(TestApiData))]
     public async Task TestGenerateApi(string api, string purpose, TestOptions options = TestOptions.None)
     {
-        this.Logger.WriteLine($"Testing {api} - {purpose}");
+        await this.TestGenerateApiWorker(api, purpose, options, "net9.0");
+    }
+
+    [Theory]
+    [MemberData(nameof(TestApiData))]
+    public async Task TestGenerateApiNet8(string api, string purpose, TestOptions options = TestOptions.None)
+    {
+        await this.TestGenerateApiWorker(api, purpose, options, "net8.0");
+    }
+
+    public async Task TestGenerateApiWorker(string api, string purpose, TestOptions options, string tfm)
+    {
+        LanguageVersion langVersion = (tfm == "net8.0") ? LanguageVersion.CSharp12 : LanguageVersion.CSharp13;
+
+        this.compilation = this.starterCompilations[tfm];
+        this.parseOptions = this.parseOptions.WithLanguageVersion(langVersion);
+        this.Logger.WriteLine($"Testing {api} - {tfm} - {purpose}");
         this.nativeMethods.Add(api);
-        await this.InvokeGeneratorAndCompile($"Test_{api}", options);
+        await this.InvokeGeneratorAndCompile($"Test_{api}_{tfm}", options);
     }
 
     [Fact]
